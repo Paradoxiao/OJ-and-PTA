0e0ef2a09ca0a592a77309948141b843a42e447a
Paradoxiao <1741841452@qq.com>
2025-06-12 19:03:13 +0800

    PTA H15 标记位判环

--- C++/PTA/H15/c.cpp
+++ C++/PTA/H15/c.cpp
@@ -45,6 +45,7 @@ int main(void) {
   return 0;
 }
 /* 你的代码将被嵌在这里 */
+/*
 void dfs(LGraph graph, int v, int *visit, int *back, int *visited, int *time) {
   visit[v] = ++(*time);
   visited[v] = 1;
@@ -65,6 +66,27 @@ bool IsAcyclic(LGraph graph) {
   for (int i = 0; i < graph->n_verts; ++i)
     for (Position p = graph->ver_list[i]->adj; p != NULL; p = p->next)
       if (visit[i] > visit[p->dest] && back[p->dest] > back[i])
+        return false;
+  return true;
+}
+*/
+// HACK: 技巧:标记位判环
+bool dfs(LGraph graph, int v, int *state) {
+  state[v] = 1;
+  for (Position p = graph->ver_list[v]->adj; p != NULL; p = p->next) {
+    if (state[p->dest] == 1)
+      return false;
+    if (state[p->dest] == 0)
+      dfs(graph, p->dest, state);
+  }
+  state[v] = 2;
+  return 1;
+}
+bool IsAcyclic(LGraph graph) {
+  int *state = (int *)calloc(graph->n_verts, sizeof(int));
+  for (int i = 0; i < graph->n_verts; i++)
+    if (state[i] == 0)
+      if (!dfs(graph, i, state))
         return false;
   return true;
 }
