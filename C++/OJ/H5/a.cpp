d2cdaa34207736e571599da1513efaca0c455da6
Paradoxiao <1741841452@qq.com>
2025-07-03 11:48:04 +0800

    OJ H5 补全题目

--- C++/OJ/H5/a.cpp
+++ C++/OJ/H5/a.cpp
@@ -27,3 +27,28 @@ int main() {
   cout << ",area=" << c2.getArea() << endl;
   return 0;
 }
+
+/*
+
+Description
+
+定义Circle类，有一个私有的、double类型的属性radius，表示圆的半径。有一个公有的、double类型的常静态数据成员PI=3.14，表示圆周率。定义getArea()和getRadius()方法，返回圆的面积和半径。注意：在定义上述两个方法时，要保证程序能正确执行。
+Input
+
+一个double类型的数据。
+Output
+
+见样例。
+Sample Input
+3.15
+Sample Output
+PI=3.14
+radius=2,area=12.56
+radius=3.15,area=31.1566
+HINT
+
+Append Code
+append.cc,
+[Submit][Status]
+
+*/
