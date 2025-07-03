dfecc2def7f8d1852f196f9353aa9302d92baaa3
Paradoxiao <1741841452@qq.com>
2025-07-03 11:47:58 +0800

    OJ H4 补全题目

--- C++/OJ/H4/g.cpp
+++ C++/OJ/H4/g.cpp
@@ -32,3 +32,37 @@ int main() {
   os.output();
   return 0;
 }
+/*
+
+Description
+
+设计一个存储整数的有序集合类，其中的数据是按照从小到大的顺序存储的。
+
+用C++编写OrdSet类来完成代码，调用格式见“Append Code”。
+
+OrdSet::input()按格式输入数据。
+
+OrdSet::output()按格式输出数据。
+
+STL容器：set、map、vector、deque、queue、stack、array、list等被禁用。万能头文件被禁用
+
+Input
+
+首先输入一个整数n（n>=0）。后接n个不同的整数。若n=0，则后续无输入。
+
+Output
+
+输出用“[”和“]”包括起来有序集合的元素，按从小到大的顺序。例如：空集输出为“[NULL]”，只有一个元素0的集合输出为“[0]”。集合多个元素间用一个空格隔开。
+
+Sample Input
+5
+8 2 6 1 4
+Sample Output
+[1 2 4 6 8]
+HINT
+
+Append Code
+append.cc,
+[Submit][Status]
+
+*/
--- C++/OJ/H4/h.cpp
+++ C++/OJ/H4/h.cpp
@@ -74,3 +74,46 @@ int main() {
   TestOrdSet(oo, os);
   return 0;
 }
+/*
+
+Description
+
+设计一个存储整数的有序集合类，其中的数据是按照从小到大的顺序存储的。
+
+用C++编写OrdSet类来完成代码，调用格式见“Append Code”。
+
+OrdSet::input()按格式输入数据。
+
+OrdSet::output()按格式输出数据。
+
+OrdSet::is_equal ()判断参数与自身是否相等。
+
+OrdSet::is_equal ()判断参数是否为自身的子集。
+
+OrdSet::is_equal ()判断参数是否为自身的真子集。
+
+STL容器：set、map、vector、deque、queue、stack、array、list等被禁用。万能头文件被禁用
+
+Input
+
+输入为两个集合：每个集合首先输入一个整数n（n>=0）。后接n个整数（有可能重复，重复的整数是集合中的同一个元素）。若n=0，则后续无输入。
+
+Output
+
+输出用“[”和“]”包括起来有序集合的元素，按从小到大的顺序。例如：空集输出为“[NULL]”，只有一个元素0的集合输出为“[0]”。集合多个元素间用一个空格隔开。
+
+Sample Input
+8
+6 2 6 1 2 6 1 2
+4
+6 2 1 4
+Sample Output
+[1 2 6] is subset to [1 2 4 6].
+[1 2 6] is proper subset to [1 2 4 6].
+HINT
+
+Append Code
+append.cc,
+[Submit][Status]
+
+*/
