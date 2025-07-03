aa5ea398b1fc70fedccecb7c7b94a2ea7e2c9afb
Paradoxiao <1741841452@qq.com>
2025-07-03 11:48:23 +0800

    OJ S3 补全题目

--- C++/OJ/S3/b.cpp
+++ C++/OJ/S3/b.cpp
@@ -62,3 +62,44 @@ int main() {
   Film film(directorName, actorName, filmName);
   return 0;
 }
+/*
+
+Description
+
+将电影的组成简单化，一部电影包含1个导演、1个演员和1个电影名。定义如下类：Person、Director、Actor和Film，Person类只有一个属性name，表示姓名。Director和Acotr是Person类的子类。Film包含director、actor和name三个属性，分别表示导演、演员和电影名。根据给定的主函数和输入、输出，编写这4个类，达到题目要求的效果。
+Input
+
+三个字符串，每个字符串不含空白符。分别表示导演的名字、演员的名字和电影的名字。
+Output
+
+见样例。
+Sample Input
+ZhangYimao ZhangZiyi WoHuCangLong
+Sample Output
+Person ZhangYimao is created.
+Person ZhangYimao is created.
+Director ZhangYimao is created.
+Person ZhangZiyi is created.
+Actor ZhangZiyi is created.
+Person ZhangYimao is created.
+Director ZhangYimao is created.
+Person ZhangZiyi is created.
+Actor ZhangZiyi is created.
+Film WoHuCangLong is created.
+Film WoHuCangLong is erased.
+Actor ZhangZiyi is erased.
+Person ZhangZiyi is erased.
+Director ZhangYimao is erased.
+Person ZhangYimao is erased.
+Actor ZhangZiyi is erased.
+Person ZhangZiyi is erased.
+Director ZhangYimao is erased.
+Person ZhangYimao is erased.
+Person ZhangYimao is erased.
+HINT
+
+Append Code
+append.cc,
+[Submit][Status]
+
+*/
--- C++/OJ/S3/e.cpp
+++ C++/OJ/S3/e.cpp
@@ -24,3 +24,35 @@ int main() {
   }
   return 0;
 }
+/*
+
+Description
+
+定义Point类，有2个double类型的属性表示点的横坐标和纵坐标。重载其减法运算符，用于计算两个点之间的距离。
+
+Input
+
+输入一系列点的坐标，每个点的坐标占一行。
+
+Output
+
+见样例。
+
+Sample Input
+20 20
+30 30
+40 40
+50 50
+Sample Output
+14.1421
+28.2843
+42.4264
+56.5685
+
+HINT
+
+Append Code
+append.cc,
+[Submit][Status]
+
+*/
